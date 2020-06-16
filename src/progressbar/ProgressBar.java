f347dfce60237e032221edc55dc8df0168badb84
Borislove <abirme@yandex.ru>
2020-06-16 23:21:24 +0300

    test test

---
+++ src/progressbar/ProgressBar.java
@@ -0,0 +1,10 @@
+package progressbar;
+
+public class ProgressBar {
+
+    public static void main(String[] args) {
+
+        ProgressBarDemo obj_frame = new ProgressBarDemo();
+        obj_frame.setVisible(true);
+    }
+}
